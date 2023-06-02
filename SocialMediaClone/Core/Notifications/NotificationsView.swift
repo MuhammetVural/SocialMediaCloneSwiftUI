53e62e874404734dac05352043e226f5a855e1b1
Muhammet <muhammetvural@anadolu.edu.tr>
2023-06-02 16:02:28 +0300

    card işlemi tamamlandı profile view e geçildi

---
+++ SocialMediaClone/Core/Notifications/NotificationsView.swift
@@ -0,0 +1,20 @@
+//
+//  NotificationsView.swift
+//  SocialMediaClone
+//
+//  Created by Muhammet Vural on 2.06.2023.
+//
+
+import SwiftUI
+
+struct NotificationsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NotificationsView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationsView()
+    }
+}
--- SocialMediaClone/SocialMediaCloneApp.swift
+++ SocialMediaClone/SocialMediaCloneApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct SocialMediaCloneApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MainTabView()
         }
     }
 }
