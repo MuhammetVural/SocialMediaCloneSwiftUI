2e797d2eee2ebb9a1b4e61593a65c498d19d00dc
Muhammet <muhammetvural@anadolu.edu.tr>
2023-06-02 14:13:20 +0300

    Initial Commit

---
+++ SocialMediaClone/SocialMediaCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialMediaCloneApp.swift
+//  SocialMediaClone
+//
+//  Created by Muhammet Vural on 2.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct SocialMediaCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
