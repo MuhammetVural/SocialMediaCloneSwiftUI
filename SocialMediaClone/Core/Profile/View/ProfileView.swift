b4f0d9f6878f7505c6b8d5597939017fde34d073
Muhammet <muhammetvural@anadolu.edu.tr>
2023-06-03 21:30:04 +0300

    profile tamamlandı

--- SocialMediaClone/Core/Profile/View/ProfileView.swift
+++ SocialMediaClone/Core/Profile/View/ProfileView.swift
@@ -8,11 +8,23 @@
 import SwiftUI
 
 struct ProfileView: View {
+    @State private var selectedFilter: TweetFilterViewModel = .tweets
+    @Namespace var animation
     var body: some View {
         VStack(alignment: .leading) {
             headerView
             actionButtons
             userInfoDetails
+            tweetFilterBar
+            ScrollView{
+                LazyVStack{
+                    ForEach(0 ... 20, id: \.self){ _ in
+                        TweetRowView()
+                            .padding()
+                    }
+                }
+            }
+            
             
             
             
@@ -129,5 +141,35 @@ extension ProfileView{
             
             
         }.padding(.horizontal)
+    }
+    var tweetFilterBar: some View {
+        HStack{
+            ForEach(TweetFilterViewModel.allCases, id: \.rawValue){
+                item in
+                VStack{
+                    Text(item.title)
+                    .font(.subheadline)
+                    .fontWeight(selectedFilter == item ? .semibold : .regular)
+                    .foregroundColor(selectedFilter == item ? .black : .gray)
+                    
+                    if selectedFilter == item {
+                        Capsule()
+                            .foregroundColor(Color(.systemBlue))
+                            .frame(height: 3)
+                            .matchedGeometryEffect(id: "filter", in: animation)
+                    }else{
+                        Capsule()
+                            .foregroundColor(Color(.clear))
+                            .frame(height: 3)
+                        
+                    }
+                }
+                .onTapGesture {
+                    withAnimation(.easeInOut){
+                        self.selectedFilter = item
+                    }
+                }
+            }
+        }.overlay(Divider().offset(x:0, y: 16))
     }
 }
