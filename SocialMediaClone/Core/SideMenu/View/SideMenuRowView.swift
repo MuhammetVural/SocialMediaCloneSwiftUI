f21d2ffa8a1b9707fed1e351912c40849e95fadd
Muhammet <muhammetvural@anadolu.edu.tr>
2023-06-04 17:56:30 +0300

    sidemenuview tamamlandı

---
+++ SocialMediaClone/Core/SideMenu/View/SideMenuRowView.swift
@@ -0,0 +1,20 @@
+//
+//  SideMenuRowView.swift
+//  SocialMediaClone
+//
+//  Created by Muhammet Vural on 4.06.2023.
+//
+
+import SwiftUI
+
+struct SideMenuRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SideMenuRowView_Previews: PreviewProvider {
+    static var previews: some View {
+        SideMenuRowView()
+    }
+}
