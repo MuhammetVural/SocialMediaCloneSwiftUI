eff342f068947f5747accc6f3a8398c38dfeca57
Muhammet <muhammetvural@anadolu.edu.tr>
2023-06-05 15:01:48 +0300

    Side menu tamamlandı

--- SocialMediaClone/ContentView.swift
+++ SocialMediaClone/ContentView.swift
@@ -8,19 +8,50 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showMenu = false
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack(alignment: .topLeading) {
+            MainTabView()
+                .navigationBarHidden(showMenu)
+            if showMenu{
+                ZStack{
+                    Color(.black)
+                        .opacity(showMenu ? 0.25 : 0)
+
+                }.onTapGesture {
+                    withAnimation(.easeInOut){
+                        showMenu = false
+                    }
+                }
+                .ignoresSafeArea()
+            }
+            SideMenuView()
+                .frame(width: 300)
+                .background(showMenu ? Color.white : Color.clear)
+                .offset(x: showMenu ? 0 : -300, y:0)
+                
         }
-        .padding()
+        .navigationTitle("Home")
+        .navigationBarTitleDisplayMode(.inline)
+        .toolbar{
+            ToolbarItem(placement: .navigationBarLeading){
+                Button{
+                    withAnimation{
+                        showMenu.toggle()
+                    }
+                } label: {
+                    Circle()
+                        .frame(width: 32, height: 32)
+                }
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        NavigationView{
+            ContentView()
+        }
     }
 }
