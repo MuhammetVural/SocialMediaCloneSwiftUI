fb5e19e2a2d0139acb651515df289bac7dd1d247
Muhammet <muhammetvural@anadolu.edu.tr>
2023-06-08 13:33:24 +0300

    Upload Tweet sayfası oluşturuldu tasarım tamamlandı cancel butonu atifleştirildi logine geçildi

---
+++ SocialMediaClone/Core/Authentication/Views/RegistrationView.swift
@@ -0,0 +1,20 @@
+//
+//  RegistrationView.swift
+//  SocialMediaClone
+//
+//  Created by Muhammet Vural on 8.06.2023.
+//
+
+import SwiftUI
+
+struct RegistrationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RegistrationView_Previews: PreviewProvider {
+    static var previews: some View {
+        RegistrationView()
+    }
+}
--- SocialMediaClone/Core/Feed/View/FeedView.swift
+++ SocialMediaClone/Core/Feed/View/FeedView.swift
@@ -37,7 +37,7 @@ struct FeedView: View {
             .clipShape(Circle())
             .padding()
             .fullScreenCover( isPresented: $showNewTweetView){
-                Text("New Tweet view..")
+                NewTweetView()
             }
             
         }
