226f835ddc5d567485997ae990ad526035a6d412
Muhammet <muhammetvural@anadolu.edu.tr>
2023-06-08 17:37:46 +0300

    Login ekranı devam ediyor

--- SocialMediaClone/SocialMediaCloneApp.swift
+++ SocialMediaClone/SocialMediaCloneApp.swift
@@ -12,7 +12,8 @@ struct SocialMediaCloneApp: App {
     var body: some Scene {
         WindowGroup {
             NavigationView{
-                ContentView()
+                //ContentView()
+                LoginView()
             }
         }
     }
